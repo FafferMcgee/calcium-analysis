53706fe0daa3298249476a4277b90f17915346da
Michael Schamber <schamber.m@gmail.com>
2018-12-11 13:29:28 -0600

    Updated packages installed

--- PackageInstaller.R
+++ PackageInstaller.R
@@ -3,16 +3,13 @@ install.packages( c('devtools',
                    'MESS',
                    'Sandwich',
                    'ggplot2',
-                   'dplyr', 
+                   'dplyr',
                    'tidyr',
                    'lmtest',
                    'multcomp',
                    'plyr',
-                   'bio3d',
                    'shiny',
                    'rsconnect',
-                   'muscle',
-                   'shiny',
                    'knitr',
                    'data.table',
                    'Cairo'
@@ -21,11 +18,5 @@ install.packages( c('devtools',
 library('devtools')
 
 install_github('FafferMcGee/Calcium-Analysis')
-install_git('https://git.bioconductor.org/packages/muscle')
 
 
-# Installation of biomaRt as of 20181105
-# Taken from: https://bioconductor.org/packages/release/bioc/html/biomaRt.html
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("biomaRt", version = "3.8")
