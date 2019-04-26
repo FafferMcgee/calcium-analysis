84d1a3749ec3f1cb54ff31d6e7ee8979056a0476
Michael Schamber <schamber.m@gmail.com>
2019-04-26 18:42:23 -0500

    Prototyping new function

---
+++ R/maxF_Fo.R
@@ -0,0 +1,28 @@
+#' A function used to quantify the response in the region of interest normalized by F/Fo.
+#'
+#' It normalizes the intensity data during stimulus window by calculating an average Fo before stimulus, and then dviding the fluorescence values in this window by the background Fo.
+#'
+#' After this normalization, it looks for the maximum value in the same stimulus window and outputs this value as the 'response'.
+#'
+#' Normalizing first by an averaged background (Fo) right before stimulus corrects for photobleaching/drift.
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+
+
+maxF_Fo <- function(data, conditions, averageWindow){
+
+
+
+
+
+}
