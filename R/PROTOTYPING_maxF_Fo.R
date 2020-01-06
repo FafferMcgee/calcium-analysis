a66d55ed8e4235299fb9e6d1a54f0913b5823d8d
Michael Schamber <schamber.m@gmail.com>
2020-01-06 11:11:31 -0600

    Bug Fixes
    Commented out prototyping file as it was a source of multiple bugs

--- R/PROTOTYPING_maxF_Fo.R
+++ R/PROTOTYPING_maxF_Fo.R
@@ -1,64 +1,64 @@
-library(dplyr)
-
-# make sure no specific links to a file are uncommented before pushing to github
-# data <-  read.csv('file:///H:/CellBasedImaging/RezaLab/20190425MRS_WT_Casr_CalciumTitrations/Data/WholeImage_Calcium_Experiment139.csv')
-
-conditions <- c(30, 50, 75, 95, 135, 155, 205, 225, 270, 290, 365, 385, 455, 475, 570, 590, 645, 665)
-
-data <- data %>%
-  dplyr::select(X,IntDen1)
-
-averageWindow = 15
-
-responseVector <- rep(NA,((length(conditions))/2)+1 )
-
-for (i in seq_along(conditions[-length(conditions)])){ # iterate along the indexes of conditions, but omitting the final value
-
-  # calculate the indexes of the background region to calculate Fo
-  frame1 <- conditions[i]-averageWindow+1
-  frame2 <- conditions[i]
-
-  # calculate Fo by finding average between the two frames
-  Fo <-  mean(data$IntDen1[frame1:frame2])
-
-
-  response <- max((data$IntDen1[conditions[i]:conditions[i+1]] / Fo))
-
-
-  #responseVector[i] <- response
-  #return(responseVector)
-
-  # print(i)
-  # print(conditions[i]-averageWindow+1)
-  # print(conditions[i])
-  print(Fo)
-  print(response)
-  # print(frame1)
-  # print(frame2)
-
-}
-
-
-
-
-test2 <- c(data$IntDen1[conditions[1]-averageWindow+1:conditions[1]])
-
-mean(test2)
-
-mean(data$IntDen1[conditions[14]-averageWindow+1:conditions[14]])
-
-data$IntDen1[conditions[14]-averageWindow+1]
-data$IntDen1[conditions[14]]
-
-data$IntDen1[conditions[14]-averageWindow+1:conditions[14]]
-
-
-
-
-
-
-
-
-
-
-
+# library(dplyr)
+#
+# # make sure no specific links to a file are uncommented before pushing to github
+# # data <-  read.csv('file:///H:/CellBasedImaging/RezaLab/20190425MRS_WT_Casr_CalciumTitrations/Data/WholeImage_Calcium_Experiment139.csv')
+#
+# conditions <- c(30, 50, 75, 95, 135, 155, 205, 225, 270, 290, 365, 385, 455, 475, 570, 590, 645, 665)
+#
+# #data <- data %>%
+# #  dplyr::select(X,IntDen1)
+#
+# averageWindow = 15
+#
+# responseVector <- rep(NA,((length(conditions))/2)+1 )
+#
+# for (i in seq_along(conditions[-length(conditions)])){ # iterate along the indexes of conditions, but omitting the final value
+#
+#   # calculate the indexes of the background region to calculate Fo
+#   frame1 <- conditions[i]-averageWindow+1
+#   frame2 <- conditions[i]
+#
+#   # calculate Fo by finding average between the two frames
+#   Fo <-  mean(data$IntDen1[frame1:frame2])
+#
+#
+#   response <- max((data$IntDen1[conditions[i]:conditions[i+1]] / Fo))
+#
+#
+#   #responseVector[i] <- response
+#   #return(responseVector)
+#
+#   # print(i)
+#   # print(conditions[i]-averageWindow+1)
+#   # print(conditions[i])
+#   print(Fo)
+#   print(response)
+#   # print(frame1)
+#   # print(frame2)
+#
+# }
+#
+#
+#
+#
+# test2 <- c(data$IntDen1[conditions[1]-averageWindow+1:conditions[1]])
+#
+# mean(test2)
+#
+# mean(data$IntDen1[conditions[14]-averageWindow+1:conditions[14]])
+#
+# data$IntDen1[conditions[14]-averageWindow+1]
+# data$IntDen1[conditions[14]]
+#
+# data$IntDen1[conditions[14]-averageWindow+1:conditions[14]]
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- R/integrateF_Fo.R
+++ R/integrateF_Fo.R
@@ -9,8 +9,8 @@
 #'
 #'
 #' Inputs:
-#' @Data = Dataframe containing fluorescence intensity data
-#' @Conditions = vectory of integers indicating when
+#' @data = Dataframe containing fluorescence intensity data
+#' @conditions = vectory of integers indicating when
 #' @averageWindow = the window to use prior to stimulus for calculating Fo
 #'
 #' i = odd index of conditions (corresponds of start of new condition)
